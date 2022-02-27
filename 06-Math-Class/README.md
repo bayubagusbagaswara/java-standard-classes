48328de3c609bc8fa789314dc34bbc7e8d6c8a50
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-27 13:18:25 +0700

    Math class

---
+++ 06-Math-Class/README.md
@@ -0,0 +1,3 @@
+# Math Class
+
+- Class Math merupakan class utilities yang berisikan banyak sekali static method untuk operasi numerik, seperti trigonometric, logarithm, akar pangkat, dan lain-lain
---
+++ 06-Math-Class/src/main/java/com/java/MathApp.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class MathApp {
+
+    public static void main(String[] args) {
+
+        // Math adalah class yang methodnya static jadi bisa diakses langsung
+        var min = Math.min(1000, 2000);
+        System.out.println(min);
+
+        // memilih nilai maximal
+        var max = Math.max(1000,  5000);
+        System.out.println(max);
+
+        // Absolut
+        // PI
+        System.out.println(Math.PI);
+    }
+}
